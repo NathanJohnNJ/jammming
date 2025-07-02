f361b9e8e23b67d8d102cb8c63adb80a1b42a7a2
Nathan John <nj@njtd.xyz>
2025-07-02 13:46:44 +0100

    improved styling

--- src/pages/Overview.jsx
+++ src/pages/Overview.jsx
@@ -5,19 +5,19 @@ import CompactPlaylist from '../components/CompactPlaylist';
 export default function Overview(){
   
   return (
-     <section className="w-full h-[95vh] text-center" id="overview">
-      <div className="flex w-full h-full">
+     <section className="w-full h-[90vh] text-center" id="overview">
+      <div className="flex w-full h-[95%]">
         <div className="flex flex-col border-2 m-2 h-[95%] w-[40%] bg-white/85 rounded-md items-center justify-around">
           <h2 className="text-2xl font-black headers">PROFILE</h2>
           <div className="flex flex-col items-center h-full">
             <Profile />       
           </div>
         </div>
-        <div className="flex flex-col m-2 h-full w-[30%] p-2 border-2 bg-white/85 rounded-md">
+        <div className="flex flex-col m-2 h-[95%] w-[30%] p-2 border-2 bg-white/85 rounded-md">
           <h2 className="text-2xl font-black headers">NOW PLAYING</h2>
           {/* <MiniPlayer /> */}
         </div>
-        <div className="flex flex-col m-2 h-full w-[25%] border-2 bg-white/85 rounded-md">
+        <div className="flex flex-col m-2 h-[95%] w-[25%] border-2 bg-white/85 rounded-md">
           <h2 className="text-2xl font-black headers">PLAYLIST</h2>
           <CompactPlaylist />
         </div>
