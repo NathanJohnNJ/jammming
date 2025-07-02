d2dde8cb6f1216e045da3436c8c8363c59adafb1
Nathan John <nj@njtd.xyz>
2025-07-02 13:29:19 +0100

    improved styling

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -28,9 +28,9 @@ export default function SearchBar(){
   }
   return (
     <div className="absolute right-0 top-[80px] rounded-2xl opacity-80 border-green-500 border-4 ">
-      <form id="search" className="bg-neutral-200 text-neutral-600 w-[20vw] flex flex-row items-center justify-between group" onSubmit={submitHandler}>
-        <input type="text" className="flex self-start group group-active:w-[60vw]"  name="search" value={searchValue} onChange={changeHandler} onClick={onClickHandler} />
-        <GoSearch className="flex self-end" onClick={onClickHandler} />
+      <form id="search" className="relative bg-neutral-200 text-neutral-600 w-[20vw] flex flex-row items-center justify-between group rounded-2xl" onSubmit={submitHandler}>
+        <input type="text" className="flex self-start group group-active:w-[60vw] rounded-2xl"  name="search" value={searchValue} onChange={changeHandler} onClick={onClickHandler} />
+        <GoSearch className="absolute right-4 top-2" onClick={onClickHandler} />
       </form>
     </div>
   )
