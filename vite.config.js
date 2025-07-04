b0c1381a518bb1b78d1342747ea51b95f3cf0ac8
Nathan John <nj@njtd.xyz>
2025-07-04 23:05:46 +0100

    Switched redirect options back to production mode

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,20 @@
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
-import basicSsl from '@vitejs/plugin-basic-ssl';
+// import basicSsl from '@vitejs/plugin-basic-ssl';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(() => {
   return {
     server: {
-      https: true,
+      // https: true,
       cors: true,
     },
     plugins: [
-      basicSsl({
-      name: 'test',
-      domains: ['*.njtd.xyz'],
-      certDir: '/Users/.../.devServer/cert',
-    }),
+    //   basicSsl({
+    //   name: 'test',
+    //   domains: ['*.njtd.xyz'],
+    //   certDir: '/Users/.../.devServer/cert',
+    // }),
       react(),
       tailwindcss()
     ],
